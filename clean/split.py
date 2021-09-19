dd90a5baea1dc645b3f50ecb97164aab3588367e
jopvivi <70073104+jopvivi@users.noreply.github.com>
2021-09-19 13:02:25 -0500

    Update split.py

--- clean/split.py
+++ clean/split.py
@@ -1,11 +1,11 @@
 lines_per_file = 8900
 smallfile = None
-with open('Final Report.txt') as bigfile:
+with open('BIGFILE_INPUT.txt') as bigfile:
     for lineno, line in enumerate(bigfile):
         if lineno % lines_per_file == 0:
             if smallfile:
                 smallfile.close()
-            small_filename = 'Final_Report_{}.txt'.format(lineno + lines_per_file)
+            small_filename = 'SMALL_FILE_OUTPUT_{}.txt'.format(lineno + lines_per_file)
             smallfile = open(small_filename, "w")
         smallfile.write(line)
     if smallfile:
