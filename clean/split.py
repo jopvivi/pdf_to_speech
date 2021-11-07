d5c853933b7465a86a08a2e57b8096a6ca88d445
jopvivi <70073104+jopvivi@users.noreply.github.com>
2021-11-07 17:54:36 -0600

    automated

--- clean/split.py
+++ clean/split.py
@@ -1,12 +1,24 @@
+import os
+import glob
+
 lines_per_file = 8900
 smallfile = None
-with open('BIGFILE_INPUT.txt') as bigfile:
-    for lineno, line in enumerate(bigfile):
-        if lineno % lines_per_file == 0:
-            if smallfile:
-                smallfile.close()
-            small_filename = 'SMALL_FILE_OUTPUT_{}.txt'.format(lineno + lines_per_file)
-            smallfile = open(small_filename, "w")
-        smallfile.write(line)
-    if smallfile:
-        smallfile.close()
+
+cwd = os.getcwd()
+file_pattern = '*.txt'
+file_name_list = []
+
+if '*' in file_pattern:
+    file_name_list.extend(glob.glob(file_pattern))
+
+for target in file_name_list:
+	with open(target) as bigfile:
+	    for lineno, line in enumerate(bigfile):
+	        if lineno % lines_per_file == 0:
+	            if smallfile:
+	                smallfile.close()
+	            small_filename = str((lineno + lines_per_file)) + ' ' + target
+	            smallfile = open(small_filename, "w")
+	        smallfile.write(line)
+	    if smallfile:
+	        smallfile.close()
