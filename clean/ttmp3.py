f1956b9acff5e19f7663071279ef7f78002a3aad
jopvivi <70073104+jopvivi@users.noreply.github.com>
2021-11-24 03:31:22 -0600

    Added text to mp3 script.
    hangs up with large files
    to do: fix

---
+++ clean/ttmp3.py
@@ -0,0 +1,33 @@
+import os
+import glob
+import pyttsx3
+
+engine = pyttsx3.init()
+
+file_pattern = '*.txt'
+file_name_list = []
+
+if '*' in file_pattern:
+    file_name_list.extend(glob.glob(file_pattern))
+
+# speech rate
+engine.setProperty('rate', 600)
+rate = engine.getProperty('rate')
+# speech volume
+volume = engine.getProperty('volume')
+engine.setProperty('volume',1.0)
+# speech voice
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+engine.runAndWait() # engine start
+
+for target in file_name_list:
+    string_to_parse = ''
+    with open(target,'r',encoding='utf-8', errors='ignore') as text_file:
+        for i in text_file.readlines():
+            string_to_parse = string_to_parse + i
+            mp3_filename = target.replace('.txt','.mp3')
+            fullPath = os.path.join(os.getcwd(), mp3_filename)
+            engine.save_to_file(string_to_parse, fullPath)
+            engine.runAndWait()
